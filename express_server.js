91a2a859edf648806beff2eb4b12ccf017bd0104
Kevin Lee <kevin.skyulee@gmail.com>
2020-09-01 19:52:30 +0000

    added route to add new urls

--- express_server.js
+++ express_server.js
@@ -10,20 +10,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//GET root directory
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
+//GET urls to render url list into urls_index
 app.get("/urls", (req, res) => {
   let urlList = { urls: urlDatabase };
   res.render("urls_index", urlList);
 });
 
+//GET new route to render urls_new
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//GET requested shortURL with its corresponding longURL and render both onto urls_show 
 app.get("/urls/:shortURL", (req, res) => {
   let requestedURL = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", requestedURL);
 });
 
+//Server listen to PORT which is 8080 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port: ${PORT}`);
 });
